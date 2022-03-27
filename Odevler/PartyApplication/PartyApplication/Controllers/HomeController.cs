b0872d4fcb59a7c291e7f1dc5454d6d45f9564cf
Cihan Kavuk <44194232+cihannk@users.noreply.github.com>
2022-03-27 21:20:08 +0300

    Party App
    basic party invitation app built with mvc

---
+++ Odevler/PartyApplication/PartyApplication/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PartyApplication.Models;
+
+namespace PartyApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Cihan";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        public IActionResult Register(PartyAttendee partyAttendee)
+        {
+            ViewBag.JoinSuccess = false;
+            if (ModelState.IsValid) {
+                if (partyAttendee.Age >= 18)
+                    ViewBag.JoinSuccess = true;
+            }
+            else
+            {
+                return View("Register");
+            }
+
+            return View("Status");
+        }
+
+    }
+}
---
+++ Odevler/PartyApplication/PartyApplication/Models/PartyAttendee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyApplication.Models
+{
+    public class PartyAttendee
+    {
+        [Required(ErrorMessage = "İsim boş olamaz")]
+        [MinLength(2, ErrorMessage = "En az iki karakter olmalı.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Yaş boş olamaz")]
+        public int Age { get; set; }
+        [Required(ErrorMessage ="Gelecek misin belirtmen gerek")]
+        public bool DoesUserCome { get; set; }
+    }
+}
